09072a72d46565ec4a6c673ac8ee840af7251148
DUONG Damien <damien.duong@soprasteria.com>
2023-12-08 11:14:10 +0100

    first commit

---
+++ src/main/java/com/example/AfestTP/AfestTpApplication.java
@@ -0,0 +1,13 @@
+package com.example.AfestTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfestTpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfestTpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AfestTP/AfestTpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AfestTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfestTpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
